125bb65db21f5c6e696c5d7ee9a45defe4cf8165
mmputraa <muhammadmukhlish20@gmail.com>
2022-02-25 17:27:44 +0700

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,24 +3,6 @@ import './App.css';
 import { Container, Navbar, Form, FormControl, Button } from 'react-bootstrap'
 
 class Navigation extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            searchText: ''
-        }
-    };
-
-   onSearchChange = e => {
-   this.setState({
-        searchText: e.target.value
-        });
-    }
-
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.onSearch(this.query.value);
-        e.currentTarget.reset();
-    }
 
     render() {
 
@@ -29,16 +11,14 @@ class Navigation extends React.Component {
                   <Navbar bg="dark" variant="dark">
                     <Container>
                         <Navbar.Brand className="navbar-brand">NEWSWORLD</Navbar.Brand>
-                        <Form className="d-flex" onSubmit={this.handleSubmit}>
+                        <Form className="d-flex">
                             <FormControl
                             type="search"
-                            onChange={this.onSearchChange}
                             placeholder="please, search here..."
                             className="me-2"
                             aria-label="Search"
-                            ref={(input) => this.query = input}
                             />
-                            <Button className="search" variant="outline-primary" type='submit'>Search</Button>
+                            <Button className="search" variant="outline-primary">Search</Button>
                         </Form>
                     </Container>
                 </Navbar>
